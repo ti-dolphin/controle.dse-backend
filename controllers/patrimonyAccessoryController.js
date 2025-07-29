5ea241706a22e12413b70372384c33c81edb4454
ti-dolphin <ti.dse01@gmail.com>
2025-07-29 10:36:14 -0300

    testando estrutura de arquivos

---
+++ controllers/patrimonyAccessoryController.js
@@ -0,0 +1,11 @@
+// const fireBaseService = require("../services/fireBaseService");
+// const PatrimonyAccessoryService = require("../services/patrimonyAccessoryService");
+// const utils = require("../utils");
+
+// class PatrimonyAccessoryController {
+
+//   static async createAccessory(req, res) {
+//   }
+// }
+
+// module.exports = PatrimonyAccessoryController;
