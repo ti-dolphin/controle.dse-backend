9b8f9121a128bf6aa52aa89d343f43be940f2e8f
ti-dolphin <ti.dse01@gmail.com>
2025-07-30 10:12:57 -0300

    user router

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const UserController = require('../controllers/UserController');
+const UserController = require('../controllers/userController');
 
 router.get('/', UserController.getMany)
 
