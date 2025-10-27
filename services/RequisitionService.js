161343d633b11169d313b2e789b642d8c48c1b27
Leonardo Pias da Rocha <rocha.pias.leonardo@gmail.com>
2025-10-26 23:19:36 -0300

    feat: enhance debug logging in filterByOnlyMyReqs method for better validation tracking

--- services/RequisitionService.js
+++ services/RequisitionService.js
@@ -49,8 +49,10 @@ class RequisitionService {
       reqIds.map(reqId => RequisitionStatusService.getPreviousStatus(reqId))
     )
 
-    console.log( 'Validations:', validations);
-    console.log( 'User:', user.CODPESSOA);
+    for (let i = 0; i < validations.length; i++) {
+      console.log('Req ID:', reqIds[i], 'Alterado por:', validations[i]?.alterado_por, 'Type:', typeof validations[i]?.alterado_por);
+      console.log( 'User:', user.CODPESSOA, typeof user.CODPESSOA);
+    }
 
     return reqIds.filter((reqIds, i) => +validations[i]?.alterado_por === +user.CODPESSOA)
   }
